1882ef06dedbbcac427b154614dcb13d57727ed2
takanoriyanagitani <takanori.yanagitani@gmail.com>
2019-12-01 09:19:56 +0900

    formatted.

--- file2pg_test.go
+++ file2pg_test.go
@@ -11,13 +11,17 @@ func TestFile2pg(t *testing.T) {
 	d, e1 := sql.Open("postgres", "")
 	defer d.Close()
 	switch e1 {
-		case nil: break
-		default:  t.Fatalf("Unable to connect to test database: %v\n", e1)
+	case nil:
+		break
+	default:
+		t.Fatalf("Unable to connect to test database: %v\n", e1)
 	}
 
 	_, e2 := d.Exec(`CREATE SCHEMA IF NOT EXISTS file2pg`)
 	switch e2 {
-		case nil: break
-		default:  t.Fatalf("Unable to create test schema file2pg: %v", e2)
+	case nil:
+		break
+	default:
+		t.Fatalf("Unable to create test schema file2pg: %v", e2)
 	}
 }
